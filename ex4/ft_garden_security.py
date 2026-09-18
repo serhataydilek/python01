23c325b7a13175409d67a353a83a3677115b2e4a
Serhat Aydilek <saydilek@k1m06s02.42istanbul.com.tr>
2026-09-18 04:31:15 +0300

    fix ex4 encapsulation

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -2,13 +2,13 @@ class Plant:
     def __init__(self, plant_name: str,
                  plant_height: float, plant_age: int) -> None:
         self.plant_name = plant_name
-        self.plant_height = plant_height
-        self.plant_age = plant_age
+        self._height = plant_height
+        self._age = plant_age
 
     def show(self) -> None:
         print(
-            f"{self.plant_name}: {round(self.plant_height, 3)}cm"
-            f", {self.plant_age} days old")
+            f"{self.plant_name}: {round(self._height, 3)}cm"
+            f", {self._age} days old")
 
     def create(self) -> None:
         print("Plant created: ", end="")
@@ -18,7 +18,7 @@ class Plant:
             print(f"{self.plant_name}: Error, height can't be negative")
             print("Height update rejected")
         else:
-            self.plant_height = height
+            self._height = height
             print(f"Height updated: {self.get_height()}cm")
 
     def set_age(self, age: int) -> None:
@@ -26,15 +26,15 @@ class Plant:
             print(f"{self.plant_name}: Error, age can't be negative")
             print("Age update rejected")
         else:
-            self.plant_age = age
+            self._age = age
             print(f"Age updated: {self.get_age()} days")
             print()
 
     def get_height(self) -> float:
-        return self.plant_height
+        return self._height
 
     def get_age(self) -> int:
-        return self.plant_age
+        return self._age
 
 
 def main() -> None:
